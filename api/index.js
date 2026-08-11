e9eec8796d90aa84c55d9c9a54dcf0f58a0a6fca
nhatvm <vuminhnhat2408@gmail.com>
2026-08-11 09:31:29 +0700

    Add diagnostic handler to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-const app = require('../server.js');
-
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    const app = require('../server.js');
+    return app(req, res);
+  } catch (err) {
+    console.error('SERVERLESS ENTRY ERROR:', err);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify({
+      error: 'Failed to execute serverless function',
+      message: err.message,
+      stack: err.stack
+    }));
+  }
+};
